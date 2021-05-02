1773325e36d82274db52874f9bbd39f6eb958617
Reet Bhamra <66404210+reetkaurbhamra@users.noreply.github.com>
2021-05-01 22:55:12 -0400

    Create back_end.py

---
+++ back_end.py
@@ -0,0 +1,6 @@
+import numpy
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, BatchNormalization, Activation
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.constraints import maxnorm
+from keras.utils import np_utils 
