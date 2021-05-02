18ac48fd1f2b7531156a9ae2f3a11d0e98cf52f8
Pegah <43738935+pegahfallah@users.noreply.github.com>
2021-05-01 23:01:07 -0400

    tool page animations

--- src/components/Tool.js
+++ src/components/Tool.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import "./Toggle.css";
 import "./Tool.css";
+import { Player, Controls } from "@lottiefiles/react-lottie-player";
 
 import { ThemeContext } from "./ThemeProvider";
 import AppBar from "./AppBar";
@@ -34,20 +35,31 @@ export default function Tool() {
     <>
       <div style={toolContainer}>
         <div className="tool-container">
-          <blockquote
-            cite="https://onlinelibrary.wiley.com/doi/full/10.1002/aisy.201900153"
-            style={text}
-          >
-            "Microplastics (MPs) are a major environmental concern due to their
-            possible impact on water pollution, wildlife, and the food chain.
-            Reliable, rapid, and high‐throughput screening of MPs from other
-            components of a water sample after sieving and/or digestion is still
-            a highly desirable goal to avoid cumbersome visual analysis by
-            expert users under the optical microscope."
-          </blockquote>
           <button className="button" style={btn} onClick={scan}>
             start scanning
           </button>
+          <div className="frame">
+            <div className="left">
+              <blockquote
+                cite="https://onlinelibrary.wiley.com/doi/full/10.1002/aisy.201900153"
+                style={text}
+              >
+                "Microplastics (MPs) are a major environmental concern due to
+                their possible impact on water pollution, wildlife, and the food
+                chain. Reliable, rapid, and high‐throughput screening of MPs
+                from other components of a water sample after sieving and/or
+                digestion is still a highly desirable goal to avoid cumbersome
+                visual analysis by expert users under the optical microscope."
+              </blockquote>
+            </div>
+            <div className="right">
+              <Player
+                autoplay
+                src="https://assets9.lottiefiles.com/temp/lf20_iRxzMr.json"
+                style={{ height: "500px", width: "500px", paddingTop: "6px" }}
+              ></Player>
+            </div>
+          </div>
         </div>
       </div>
       <Footer></Footer>
