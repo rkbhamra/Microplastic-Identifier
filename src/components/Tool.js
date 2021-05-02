e60f711b4d3905447b1f30fd6bd6fcd023942414
Pegah <43738935+pegahfallah@users.noreply.github.com>
2021-05-02 00:17:22 -0400

    modal

--- src/components/Tool.js
+++ src/components/Tool.js
@@ -2,12 +2,33 @@ import React, { useContext } from "react";
 import "./Toggle.css";
 import "./Tool.css";
 import { Player, Controls } from "@lottiefiles/react-lottie-player";
-
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import Backdrop from "@material-ui/core/Backdrop";
+import Fade from "@material-ui/core/Fade";
 import { ThemeContext } from "./ThemeProvider";
 import AppBar from "./AppBar";
 import Footer from "./Footer";
+
+const useStyles = makeStyles((theme) => ({
+  modal: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  paper: {
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 export default function Tool() {
   const state = useContext(ThemeContext);
+
+  const classes = useStyles();
+
+  // Make sure to bind modal to your appElement (https://reactcommunity.org/react-modal/accessibility/)
   const toolContainer = {
     backgroundColor: state.theme.primary,
     height: "90vh",
@@ -31,9 +52,19 @@ export default function Tool() {
     fontSize: "16px",
     transition: "1.5s",
     borderRadius: "100px",
+    marginTop: "20%",
   };
 
   const scan = () => {};
+  const [open, setOpen] = React.useState(false);
+
+  const openModal = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <>
@@ -44,9 +75,34 @@ export default function Tool() {
             <span className="rainbow-text"> Machine Learning</span> with
             Holographic Imaging
           </p>
-          <button className="button" style={btn} onClick={scan}>
-            start scanning
-          </button>
+          <div className="button-container">
+            <button className="button" style={btn} onClick={scan}>
+              start scanning
+            </button>
+            <button className="button" style={btn} onClick={openModal}>
+              check setup
+            </button>
+          </div>
+          <Modal
+            aria-labelledby="transition-modal-title"
+            aria-describedby="transition-modal-description"
+            className={classes.modal}
+            open={open}
+            onClose={handleClose}
+            closeAfterTransition
+            BackdropComponent={Backdrop}
+            BackdropProps={{
+              timeout: 500,
+            }}
+          >
+            <Fade in={open}>
+              <div className={classes.paper}>
+                <h2 id="transition-modal-title">Check Set Up</h2>
+                <p id="transition-modal-description"></p>
+              </div>
+            </Fade>
+          </Modal>
+
           <div className="frame">
             <div className="left">
               <blockquote
